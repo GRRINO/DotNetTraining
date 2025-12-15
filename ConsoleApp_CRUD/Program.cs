2a73d99963d6bf75680174041caa81a64394c362
ACE STAFF <ACE STAFF@DESKTOP-ACHQNCM>
2025-12-15 15:29:19 +0630

    Add project files.

---
+++ ConsoleApp_CRUD/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp_CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string connectionString = "Data Source=.;Initial Catalog=DotNetTraining;User ID=sa;Password=sasa@123;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            Console.WriteLine("Database Connecting");
+            connection.Open();
+
+            string query = @"SELECT[BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[IsDelete]
+  FROM[dbo].[tblBlog]";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //adapter.Fill(dt);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read()) {
+                Console.WriteLine(reader["BlogId"]);
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+            }
+
+            Console.WriteLine("Database Closing");
+            connection.Close();
+
+            //foreach(DataRow dr in dt.Rows)
+            //{
+            //    Console.WriteLine(dr["BlogId"]);
+            //    Console.WriteLine(dr["BlogTitle"]);
+            //    Console.WriteLine(dr["BlogAuthor"]);
+            //    Console.WriteLine(dr["BlogContent"]);
+            //}
+        }
+    }
+}
